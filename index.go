e11fc485d04aee3451e7117fcdb38ad2fce0f4c0
hyh <ec.huyinghuan@gmail.com>
2017-06-20 17:33:35 +0800

    add possive buzzer

--- index.go
+++ index.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"pi/work"
+	"log"
 
+	"github.com/kidoman/embd"
 	_ "github.com/kidoman/embd/host/all" // This loads the RPi driver
 )
 
 func main() {
-	work.ShowTemperature()
+	embd.InitGPIO()
+	defer embd.CloseGPIO()
+	embd.SetDirection(17, embd.Out)
+	pwm, err := embd.NewPWMPin(17)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	pwm.SetPeriod(131)
+	pwm.SetPeriod(50)
+	pwm.SetMicroseconds(1000)
 }
